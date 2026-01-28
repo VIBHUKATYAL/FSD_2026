3f8118209e524c08d30574e266b8b95c67d94582
VIBHUKATYAL <vibhukatyal75@gmail.com>
2026-01-28 16:20:38 +0530

    scriptUpdate

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 console.log("Script loaded successfully.");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.info("This is an informational message.");
