9a11f7a79eaa925011144de40116fcfc122cf45d
VIBHUKATYAL <vibhukatyal75@gmail.com>
2026-01-28 16:23:58 +0530

    scriptUpdate2

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ console.log("Script loaded successfully.");
 console.warn("This is a warning message.");
 console.error("This is an error message.");
 console.info("This is an informational message.");
+let a=10;
+let b=20;
+let a=b;
+var c=30;
+var d=40
+var c=d;
+console.log("Values"+c+a);
